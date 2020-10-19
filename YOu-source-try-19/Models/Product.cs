2840cbf8fb4b9650039fe947d6c3f253d2d09889
JacquesPienaarJJ <72506494+JacquesPienaarJJ@users.noreply.github.com>
2020-10-19 19:51:31 +0200

    initial commit of Backend, API works
    There is only implemented for the API to send products to the front end

---
+++ YOu-source-try-19/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YOu_source_try_19.Models
+{
+    public class Product
+    {
+        public int ProductID{ get; set; }
+        public int MerchantID { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+
+    }
+}
